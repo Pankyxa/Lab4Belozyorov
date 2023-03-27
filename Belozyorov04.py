b9033eda8760d79a274c6ac339a7a4687b2623f1
Pankyxa <114245476+Pankyxa@users.noreply.github.com>
2023-03-27 21:30:39 +0500

    Update Belozyorov04.py

--- Belozyorov04.py
+++ Belozyorov04.py
@@ -1,126 +1,182 @@
-﻿def pis(a):
-    b = str
-    ls1 = ('один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шетснадцать', 'семнадцать', 'восемнадцать', 'девятнадцать')
-    ls2 = ( 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семдесят', 'восемдесят', 'девяносто')
-    ls3 = ('сто', 'двести', 'триста','четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот')
-    ls4 = ('одна', 'две', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шетснадцать', 'семнадцать', 'восемнадцать', 'девятнадцать',)
+lass rabotnik:
+    def __init__(self, i, k):
+        self.num = i
+        self.kilom = k
+    def tar(self, t):
+        self.taksi = tak[t]
+        
+class taksi:
+    def __init__(self, num, t):
+        self.num=num
+        self.tarif = t
 
+n=int(input("Сколько работников поедет домой?\n"))
+rab=[]
+tak = []
+sum = 0
+l=0
+k=0
+b=100000
+i=0
+c=1
+for j in range(n):
+    kil = int(input(f"Введите расстояние до дома {j+1} работника: "))
+    rab.append(rabotnik((j+1), kil))
+for j in range(n):
+    tar= int(input(f"Введите тариф {j+1} такси за километр: "))
+    tak.append(taksi((j+1), tar))
+list.sort(rab, key=lambda rabotnik: rabotnik.kilom)
+list.sort(tak, reverse = True, key=lambda taksi: taksi.tarif)
+for j in rab:
+    j.tar(l)
+    l=l+1
+for j in range(n):
+    sum = sum + (rab[j-1].kilom * tak[j-1].tarif)
+list.sort(rab, key=lambda rabotnik: rabotnik.num)
+for j in rab:
+    print (j.num, "работник поедет на такси №", j.taksi.num)
+print ("Общая сумма затрат на дорогу домой всех сотрудников составляет:", sum)
+while k!=6:
+    c = sum // b
+    if (c == 1 and (k == 0 or k == 3)):
+        print('сто ', end='')
+        i=3
+    elif (c == 2 and (k == 0 or k == 3)):
+        print('двести ', end='')
+        i=3
+    elif (c == 3 and (k == 0 or k == 3)):
+        print('триста ', end='')
+        i=3
+    elif (c == 4 and (k == 0 or k == 3)):
+        print('четыреста ', end='')
+        i=3
+    elif (c == 5 and (k == 0 or k == 3)):
+        print('пятьсот ', end='')
+        i=3
+    elif (c == 6 and (k == 0 or k == 3)):
+        print('шестьсот ', end='')
+        i=3
+    elif (c == 7 and (k == 0 or k == 3)):
+        print('семьсот ', end='')
+        i=3
+    elif (c == 8 and (k == 0 or k == 3)):
+        print('восемьсот ', end='')
+        i=3
+    elif (c == 9 and (k == 0 or k == 3)):
+        print('девятьсот ', end='')
+        i=3
 
-    if a < 20:
-        b = ls1[a - 1]
-    elif a // 100 == 0:
-        b =  ls2[a//10 - 2]
-        if a % 10 != 0:
-            b += ' ' + ls1[a%10 - 1]
-    elif a // 1000 == 0:
-        b = ls3[a//100 - 1]
-        if a % 100 != 0:
-            b += ' ' + ls2[(a//10)%10 - 2]
-            if a % 10 != 0:
-                b += ' ' + ls1[a%10 - 1] 
+    if (c == 1 and (k == 1 or k == 4)):
+        i=4
+    elif (c == 2 and (k == 1 or k == 4)):
+        print('двадцать ', end='')
+        i=3
+    elif (c == 3 and (k == 1 or k == 4)):
+        print('тридцать ', end='')
+        i=3
+    elif (c == 4 and (k == 1 or k == 4)):
+        print('сорок ', end='')
+        i=3
+    elif (c == 5 and (k == 1 or k == 4)):
+        print('пятьдесят ', end='')
+        i=3
+    elif (c == 6 and (k == 1 or k == 4)):
+        print('шестьдесят ', end='')
+        i=3
+    elif (c == 7 and (k == 1 or k == 4)):
+        print('семьдесят ', end='')
+        i=3
+    elif (c == 8 and (k == 1 or k == 4)):
+        print('восемьдесят ', end='')
+        i=3
+    elif (c == 9 and (k == 1 or k == 4)):
+        print('девяносто ', end='')
+        i=3
 
+    if (c == 1 and k == 2 and i!=4):
+        print('одна ', end='')
+        i=1
+    elif (c == 1 and k == 5 and i!=4):
+        print('один ', end='')
+        i=1
+    elif (c == 2 and k == 2 and i!=4):
+        print('две ', end='')
+        i=2
+    elif (c == 2 and k == 5 and i!=4):
+        print('два ', end='')
+        i=2
+    elif (c == 3 and (k == 2 or k == 5)and i!=4):
+        print('три ', end='')
+        i=2
+    elif (c == 4 and (k == 2 or k == 5)and i!=4):
+        print('четыре ', end='')
+        i=2
+    elif (c == 5 and (k == 2 or k == 5)and i!=4):
+        print('пять ', end='')
+        i=3
+    elif (c == 6 and (k == 2 or k == 5)and i!=4):
+        print('шесть ', end='')
+        i=3
+    elif (c == 7 and (k == 2 or k == 5)and i!=4):
+        print('семь ', end='')
+        i=3
+    elif (c == 8 and (k == 2 or k == 5)and i!=4):
+        print('восемь ', end='')
+        i=3
+    elif (c == 9 and (k == 2 or k == 5) and i!=4):
+        print('девять ', end='')
+        i=3
+    elif (i==4 and (k==2 or k==5)):
+        if (c == 1):
+            print('одинадцать ', end='')
+            i=3
+        elif (c == 2):
+            print('двенадцать ', end='')
+            i=3
+        elif (c == 3):
+            print('тринадцать ', end='')
+            i=3
+        elif (c == 4):
+            print('четырнадцать ', end='')
+            i=3
+        elif (c == 5):
+            print('пятнадцать ', end='')
+            i=3 
+        elif (c == 6):
+            print('шестнадцать ', end='')
+            i=3
+        elif (c == 7):
+            print('семьнадцать ', end='')
+            i=3
+        elif (c == 8):
+            print('восемьнадцать ', end='')
+            i=3
+        elif (c == 9):
+            print('девятнадцать ', end='')
+            i=3
+        elif (c == 0):
+            print('десять ', end='')
+            i=3
+    
+    if (k == 5):
+        if (i==1):
+            print('рубль')
+        elif (i==2):
+            print('рубля')
+        elif (i==3):
+            print('рублей')
 
-    else:
-        c = a // 1000
-        if c < 20:
-            b = ls4[c - 1]
-        elif c // 100 == 0:
-            b =  ls2[c//10 - 2]
-            if c % 10 != 0:
-                b += ' ' + ls4[c%10 - 1]
-        elif c // 1000 == 0:
-            b = ls3[c//100 - 1]
-            if c % 100 != 0:
-                b += ' ' + ls2[(c//10)%10 - 2]
-                if c % 10 != 0:
-                    b += ' ' + ls4[c%10 - 1]
-        if str(c % 10) == '1' and c % 100 != 11:
-            b += ' тысяча'
-        elif str(c % 10) in '234':
-            b += ' тысячи'
-        else:
-            b += ' тысяч'
-        d = a % 1000
-        if d < 20:
-            if d != 0:
-                b += ' ' + ls1[d - 1]
-        elif d // 100 == 0:
-            b += ' ' + ls2[d//10 - 2]
-            if d % 10 != 0:
-                b += ' ' + ls1[d%10 - 1]
-        elif d // 1000 == 0:
-            b += ' ' + ls3[d//100 - 1]
-            if d % 100 != 0:
-                b += ' ' + ls2[(d//10)%10 - 2]
-                if d % 10 != 0:
-                    b += ' ' + ls1[d%10 - 1]
+    if (k == 2):
+        if (i==1):
+            print('тысяча ', end='')
+            i=3
+        elif (i==2):
+            print('тысячи ', end='')
+            i=3
+        elif (i==3):
+            print('тысяч ', end='')
+            i=3
 
-    if a % 10 == 1 and a % 100 != 11:
-        b += ' рубль'
-    elif str(a % 10) in '234' and str(a % 100) not in ('12', '13', '14'):
-        b += ' рубля'
-    else:
-        b += ' рублей'
-    b += '.'
-    return(b)
-
-n = int(input('Категорически вас приветствуем! Вы позвонили в такси.\nСколько вам необходимо машин?(Введите целочисленное число от 1, количество сотрудников): '))
-notx = list()
-nops = list()
-for i in range(n):
-    notx.append(i+1)
-    nops.append(i+1)
-if n == 1:
-    print('Введите', n, 'целое число, расстояние в киломметрах до дома сотрудника')
-elif n > 1 and n < 5:
-    print('Введите через пробел ', n, ' целых числа, расстояние в киломметрах до домов сотрудников')
-else:
-    print('Введите через пробел ', n, ' целых чисел, расстояние в киломметрах до домов сотрудников')
-while True:
-    km = list(map(int, input().split()))
-    if len(km) == n:
-        break
-    else:
-        print('Вы ввели неверное количество, пожалуйста повторите попытку!')
-
-if n == 1:
-    print('Введите', n, 'целое число, тариф за 1 километр')
-elif n > 1 and n < 5:
-    print('Введите через пробел ', n, ' целых числа, тариф за 1 километр')
-else:
-    print('Введите через пробел ', n, ' целых чисел, тариф за 1 километр')
-while True:
-    st = list(map(int, input().split()))
-    if len(st) == n:
-        break
-    else:
-        print('Вы ввели неверное количество, пожалуйста повторите попытку!')
-
-for i in range(len(st)-1):
-    for j in range(len(st)-i-1):
-        if int(st[j]) > int(st[j+1]):
-            st[j], st[j+1] = st[j+1], st[j]
-            notx[j], notx[j+1] = notx[j+1], notx[j]
-
-for i in range(len(km)-1):
-    for j in range(len(km)-i-1):
-        if int(km[j]) > int(km[j+1]):
-            km[j], km[j+1] = km[j+1], km[j]
-            nops[j], nops[j+1] = nops[j+1], nops[j]
-
-for i in range(n):
-    for j in range(n):
-        if nops[j] == i+1:
-            print(i+1, 'пассажир поедет на', notx[j], 'такси.')
-s = 0
-for i in range(n):
-    s += km[i]*st[i]
-
-if s % 10 == 1 and s % 100 != 11: 
-    print('Информация принята. За поездки необходимо заплатить:', s, 'рубль.')
-elif s % 10 > 1 and s % 10 < 5 and not (s % 100 > 11 and s % 100 < 15):
-    print('Информация принята. За поездки необходимо заплатить:', s, 'рубля.')
-else:
-    print('Информация принята. За поездки необходимо заплатить:', s, 'рублей.')
-
-print('Информация принята. За поездки необходимо заплатить:', pis(s))
+    sum=sum-(c*b)    
+    b = b / 10        
+    k=k+1
